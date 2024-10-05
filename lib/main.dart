b76b9f3fd7e3283dbcacb0874752352ac735b165
CaioReis29 <caio201612@gmail.com>
2024-10-04 21:19:34 -0300

    chore: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unithub/core/app/unithub_app.dart';
+import 'package:unithub/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   
   runApp(const UnithubApp());
 }
